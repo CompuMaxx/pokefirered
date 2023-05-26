bab6bb2d899fdd7e13027adc52ceb79a7ac942ff
Acimut <79547805+Acimut@users.noreply.github.com>
2023-05-26 17:08:52 -0500

    Update voicegroup012.h

--- src/data/voicegroups/voicegroup012.h
+++ src/data/voicegroups/voicegroup012.h
@@ -1,7 +1,7 @@
 
 const union VoiceGroup voicegroup012[] =
 {
-    VOICE_SQUARE_1(60, 0, 0, 2, 0, 0, 15, 0),
-    VOICE_KEYSPLIT(voicegroup_piano1, KEYSPLITTABLE_PIANO1_PTR)
+    [ACOUSTIC_GRAND_PIANO]  = DEFAULT_VOICE,
+    [BRIGHT_ACOUSTIC_PIANO] = VOICE_KEYSPLIT(voicegroup_piano1, KEYSPLITTABLE_PIANO1_PTR)
 };
 
